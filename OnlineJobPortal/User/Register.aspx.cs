020121c26878195d9b3936013d2be665f870003e
Jainil <chauhanjainil305@gmail.com>
2023-10-25 03:06:22 +0530

    first commit

---
+++ OnlineJobPortal/User/Register.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace OnlineJobPortal.User
+{
+    public partial class Register : System.Web.UI.Page
+    { 
+        SqlConnection con;
+        SqlCommand cmd;
+        string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnregister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con = new SqlConnection(str);
+                string query = @"Insert into [User] (Username, Password, Name, Address, Mobile, Email, Country) values (@Username, @Password, @Name, @Address, @Mobile, @Email, @Country)";
+                cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Username", txtUserName.Text.Trim());
+                cmd.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
+                cmd.Parameters.AddWithValue("@Name", txtFullName.Text.Trim());
+                cmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+                cmd.Parameters.AddWithValue("@Mobile", txtMobile.Text.Trim());
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
+                cmd.Parameters.AddWithValue("@Country", ddlCountry.SelectedValue);
+
+                con.Open();
+                int r = cmd.ExecuteNonQuery();
+                if (r > 0)
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Registered Successfull!";
+                    lblMsg.CssClass = "alert alert-success";
+                    Clear();
+                }
+                else
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Cannot save record right now, please try after sometime..!";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch(SqlException ex) { 
+                if(ex.Message.Contains("Violation of UNIQUE KEY constraint"))
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "<b>" + txtUserName.Text.Trim()+ "</b> username is already exists, try new one..! ";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+                else
+                {
+                    Response.Write("<script>alert('" + ex.Message + "');</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void Clear()
+        {
+            txtUserName.Text = string.Empty;
+            txtPassword.Text = string.Empty;
+            txtFullName.Text = string.Empty;
+            txtAddress.Text = string.Empty;
+            txtMobile.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtConfirmPassword.Text = string.Empty;
+            ddlCountry.ClearSelection();
+        }
+    }
+}
